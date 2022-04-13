39d637c3b672733faca797b188cb3cec2be7931f
hossein <92458450+Inshanecr@users.noreply.github.com>
2022-04-13 19:25:26 +0430

    Update fun.py

--- fun.py
+++ fun.py
@@ -19,7 +19,7 @@ class Style:
     UNDERLINE = '\033[4m'
     BOLD = '\033[1m'
     END = '\033[0m'
-
+    CYAN = '\033[96m'
 
 # print(f"\ndate : {datetime.now()}")
 # print(Fore.RED + "[1] " + Fore.GREEN + "Show your ip [ Use vpn ]")
@@ -33,22 +33,47 @@ while True:
 
     
     try:
-        print("""_   _    _    ____  _   _ _____     ___  _____ ___  ____  
+        print(Style.CYAN+"""_   _    _    ____  _   _ _____     ___  _____ ___  ____  
 | | | |  / \  / ___|| | | |_ _\ \   / / \|_   _/ _ \|  _ \ 
 | |_| | / _ \ \___ \| |_| || | \ \ / / _ \ | || | | | |_) |
 |  _  |/ ___ \ ___) |  _  || |  \ V / ___ \| || |_| |  _ < 
 |_| |_/_/   \_\____/|_| |_|___|  \_/_/   \_\_| \___/|_| \_\
-""")
-        print(Fore.RESET + f"\ndate : {datetime.now()}\r")
-        print(Fore.RED + "[1] " + Fore.YELLOW + "Show your ip " + Fore.GREEN + "[ Use vpn ]")
-        print(Fore.RESET + Fore.RED +"[2] " + Fore.YELLOW + "Password Generator")
-        print(Fore.RED + "[3] " + Fore.YELLOW + "Show your Area Number ")
-        print(Fore.RED + "[4] " + Fore.YELLOW + "Calculate number")
-        print(Fore.RED + "[5] " + Fore.YELLOW + "Fun")
+""")    
+        print(Fore.RESET+Style.BOLD + f"\ndate : {datetime.now()}\r")
+        print(Fore.RED+"[1]"+Fore.YELLOW+" Check site directory ")
+        print(Fore.RED + "[2] " + Fore.YELLOW + "Show your ip " + Fore.GREEN + "[ Use vpn ]")
+        print(Fore.RESET + Fore.RED +"[3] " + Fore.YELLOW + "Password Generator")
+        print(Fore.RED + "[4] " + Fore.YELLOW + "Show your Area Number ")
+        print(Fore.RED + "[5] " + Fore.YELLOW + "Calculate number")
+        print(Fore.RED + "[6] " + Fore.YELLOW + "Fun")  
         print("")
         a = input(Fore.CYAN + "Which tools you need: " + Fore.LIGHTMAGENTA_EX)
         print(Fore.RESET)
         if a == "1":
+            url = ("https://"+input(Fore.GREEN+"url :"+Fore.RED+"\n[without https://]\n"))
+            list =("""
+
+BingSiteAuth.xml
+archiveindex.html
+robots.txt
+robot.txt
+sitemap.xml
+admin-ANTIGO
+admin-admin
+admin-ajax.php
+admin-area
+admin-authz.xml
+admin-bin
+admin-cgi
+""").split()
+
+            for i in list:   
+                 http = get(url+"/"+i)
+                 if http.status_code == 200:
+                     print (Fore.GREEN+"[+]"+url+"/"+i)
+                 else:
+                     print (Fore.RED+"[!]"+url+"/"+i)
+        elif a == "2":
             ip = get("https://api.ipify.org").text
             print('\n' + Fore.MAGENTA + ip + Fore.RESET)
             http = get("https://api.ipify.org").text
@@ -56,7 +81,7 @@ while True:
             if cou == "IR":
                 print(Fore.LIGHTBLACK_EX + "Your IP has " + Fore.LIGHTRED_EX + Style.BOLD + "BLOCKED" + Style.END + " from server  " + Fore.LIGHTBLUE_EX + "[ Turn on VPN ] " + Fore.RESET + '|' + Fore.RED + " Iranian"+ Fore.WHITE +" IP " + Style.UNDERLINE + Fore.GREEN + cou + Style.END + Fore.RESET)
                 
-        elif a == "2":
+        elif a == "3":
             try:
                 length = int(input("Enter length of password: "))
             except ValueError:
@@ -71,11 +96,11 @@ while True:
                 password = "".join(sample(all,length))
                 print(Fore.GREEN + "Your pass is: " + Fore.YELLOW + "|--> "+ Style.UNDERLINE + password + Style.END + " <--|" + Fore.GREEN)
             pass
-        elif a == "3":
+        elif a == "4":
             numbers = input("Enter Number\t \nWith code area(+98):")
             ph_number = parse(numbers)
             print(geocoder.description_for_number(ph_number,"fa"))
-        elif a == "4":
+        elif a == "5":
             print("""------------------------T.me/LooQaat--------------------""")
             num_1 = int(input("Send first number to calc: "))
             num_2 = int(input("Send second number to calc: "))
@@ -94,12 +119,14 @@ while True:
         exit(1)
     else:
         try:
-            if a == "5":
+            if a == "6":
                 for i in count(2,2):
                     if i == 198618:
+                        print(Style.CYAN+"\nscrapping your file")
                         break
                     else:
                         print(i)
         except(RuntimeError,KeyboardInterrupt):
             print(" ", end="", flush=True)
+            
             continue
